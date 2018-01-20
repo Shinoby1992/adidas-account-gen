b84b4be3df5f254c5f8816c8fff4ac5612130e79
ryan9918 <ryan9918@users.noreply.github.com>
2018-01-20 16:29:33 +0000

    fuck proxy cartel

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from random import *
 from time import sleep
 import string
 import _thread
+import json
 from flask import Flask, render_template, request, redirect
 from datetime import datetime
 import logging
-import json
 import webbrowser
+import sys
 
-from utils import n_logging, c_logging
+from utils import Logger
 
 
 tokens = []
@@ -54,7 +55,7 @@ def solve():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-	token = request.form.get('g-recaptcha-response', '')
+	token = request.form['g-recaptcha-response']
 	captureToken(token)
 	return redirect("http://fuckrsvpkingz.adidas.co.uk:5000/solve", code=302)
 
@@ -70,37 +71,6 @@ class Generator():
 			self.domain = '.com'
 			self.language = 'en_US'
 		self.sitekey = sitekey
-		self.pageurl = pageurl
-		self.delay = 2
-
-	def __get_captcha_id(self):
-		params = {
-			'googlekey': self.sitekey,
-			'pageurl': self.pageurl,
-			'method': 'userrecaptcha',
-			'key': self.apikey
-		}
-		r = requests.post('http://2captcha.com/in.php', params=params)
-		captcha_id = r.text.split('|')[1]
-		return captcha_id
-
-	def __get_captcha_token(self, captcha_id):
-		params = {
-			'id': captcha_id,
-			'action': 'get',
-			'key': self.apikey
-		}
-		r = requests.get('http://2captcha.com/res.php', params=params)
-		while 'CAPCHA_NOT_READY' in r.text:
-			sleep(self.delay)
-			r = requests.get('http://2captcha.com/res.php', params=params)
-		captcha_token = r.text.split('|')[1]
-		return captcha_token
-
-	def fetch_token(self):
-		captcha_id = self.__get_captcha_id()
-		captcha_token = self.__get_captcha_token(captcha_id)
-		return captcha_token
 
 	def create_account(self, email, password, captcha_token):		
 		headers = {
@@ -151,35 +121,47 @@ class Generator():
 
 
 if __name__ == '__main__':
+	logger = Logger()
+	logger.log("Adidas Account Creator v1.1.0")
+	logger.log("@ryan9918_")
+	logger.log("***************************************************************************")
 	with open('config.json') as file:
 		config = json.load(file)
 		file.close()
+	try:
+		with open('C:\Windows\System32\drivers\etc\hosts') as file:
+			if "127.0.0.1 fuckrsvpkingz.adidas.co.uk" not in file.read():
+				with open('C:\Windows\System32\drivers\etc\hosts', 'a') as file:
+					file.write("\n\n# Adidas Account Generator")
+					file.write("\n127.0.0.1 fuckrsvpkingz.adidas.co.uk")
+					file.close()
+			else:
+				file.close()
+	except PermissionError:
+		logger.error("Failed to edit hosts file. Make sure to run as admin or edit hosts file manually.")
+		sys.exit()
 	_thread.start_new_thread(app.run, ())
 	_thread.start_new_thread(manageTokens, ())
 	accountsList = []
-	n_logging("Adidas Account Creator v1.0.0")
-	n_logging("@TheRealChefUK")
-	n_logging("***************************************************************************")
 	creator = Generator(config['locale'], '6LdyFRkUAAAAAF2YmQ9baZ6ytpVnbVSAymVpTXKi', 'https://www.adidas.com')
 	num = input("# ACCOUNTS: ")
 	webbrowser.open('http://fuckrsvpkingz.adidas.co.uk:5000/solve')
-	n_logging("Started account generator.")
+	logger.status("Started account generator.")
 	for x in range(int(num)):
 		email = '{}-{}@{}'.format(config['prefix'], randint(1111,999999999), config['domain'])
 		allchar = string.ascii_letters + string.digits
 		passw = "".join(choice(allchar) for x in range(randint(8, 12)))
-		n_logging("Task {} - Waiting for captcha token.".format(x))
-		# token = creator.fetch_token()
+		logger.warn("Task {} - Waiting for captcha token.".format(x))
 		token = sendToken()
-		n_logging("Task {} - Obtained captcha token.".format(x))
+		logger.log("Task {} - Obtained captcha token.".format(x))
 		result, account = creator.create_account(email, passw, token)
 		if result:
-			n_logging("Task {} - Created account {}".format(x, account))
+			logger.success("Task {} - Created account {}".format(x, account))
 			accountsList.append(account)
 		else:
-			n_logging("Task {} - Failed to create account.".format(x))
+			logger.error("Task {} - Failed to create account.".format(x))
 	with open('accounts.txt', 'w') as file:
 		for item in accountsList:
 			file.write('{}\n'.format(item))
 		file.close()
-	n_logging("Saved accounts to txt file.")
+	logger.status("Saved accounts to txt file.")
